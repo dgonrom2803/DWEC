f583c31121c38dce53eea50650b77b5cefae838d
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-11-22 12:58:12 +0100

    Ejercicio Cesta

---
+++ tema-08/actividades/productos.js
@@ -0,0 +1,8 @@
+var productos = [
+    [1, "Bolso", 20, "https://source.unsplash.com/random/500x500/?handbag"],
+    [2, "Móvil", 120, "https://source.unsplash.com/random/500x500/?iphone"],
+    [3, "Taza", 10, "https://source.unsplash.com/random/500x500/?cup"],
+    [4, "Zapatos", 50.5, "https://source.unsplash.com/random/500x500/?shoe"],
+    [5, "Vestido", 30, "https://source.unsplash.com/random/500x500/?dress"],
+    [6, "Cámara", 100, "https://source.unsplash.com/random/500x500/?camera"]
+]
