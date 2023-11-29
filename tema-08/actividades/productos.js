24d6795885ad56f4e40ac3e7cd626f3a247bdd8e
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-11-29 13:04:36 +0100

    Pruebas Calculo Cesta

--- tema-08/actividades/productos.js
+++ tema-08/actividades/productos.js
@@ -1,8 +1,8 @@
-var productos = [
-    [1, "Bolso", 20, "https://source.unsplash.com/random/500x500/?handbag"],
-    [2, "Móvil", 120, "https://source.unsplash.com/random/500x500/?iphone"],
-    [3, "Taza", 10, "https://source.unsplash.com/random/500x500/?cup"],
-    [4, "Zapatos", 50.5, "https://source.unsplash.com/random/500x500/?shoe"],
-    [5, "Vestido", 30, "https://source.unsplash.com/random/500x500/?dress"],
-    [6, "Cámara", 100, "https://source.unsplash.com/random/500x500/?camera"]
-]
+this.productos = [
+    new Producto(1, "Bolso", 20, "https://source.unsplash.com/random/100x100/?handbag"),
+    new Producto(2, "Móvil", 120, "https://source.unsplash.com/random/100x100/?iphone"),
+    new Producto(3, "Taza", 10, "https://source.unsplash.com/random/100x100/?cup"),
+    new Producto(4, "Zapatos", 50.5, "https://source.unsplash.com/random/100x100/?shoe"),
+    new Producto(5, "Vestido", 30, "https://source.unsplash.com/random/100x100/?dress"),
+    new Producto(6, "Cámara", 100, "https://source.unsplash.com/random/100x100/?camera"),
+  ];
