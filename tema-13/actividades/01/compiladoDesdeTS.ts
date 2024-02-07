fc81bc53a0db7a8fbe110cc3da058c3e3d0c146a
dgonrom2803 <115530984+dgonrom2803@users.noreply.github.com>
2024-02-07 20:46:58 +0100

    Act13

---
+++ tema-13/actividades/01/compiladoDesdeTS.ts
@@ -0,0 +1,43 @@
+interface BotonInterface {
+    add(): void;
+    rest(): void;
+}
+
+class GrupoBot implements BotonInterface {
+    private contador: number;
+
+    constructor() {
+        this.contador = 0;
+        $('#addButton').click(this.add.bind(this));
+        $('#restButton').click(this.rest.bind(this));
+    }
+
+    add() {
+        this.contador++;
+        const boton = new Boton(this.contador); // Instancia del Boton
+        $('#buttonsContainer').append(boton.element); // Agregar directamente el elemento del botón al contenedor
+    }
+
+    rest() {
+        if (this.contador > 0) {
+            this.contador--;
+            $('#buttonsContainer button:last-child').remove();
+        }
+    }
+}
+
+class Boton {
+    element: JQuery<HTMLElement>; // Propiedad para almacenar el elemento jQuery del botón
+
+    constructor(private contador: number) {
+        const button = $('<button>').text('Botón ' + this.contador);
+        button.click(() => {
+            alert('¡Hola desde el botón ' + this.contador + '!');
+        });
+        this.element = button; // Guardar una referencia al elemento del botón creado
+    }
+}
+
+$(document).ready(function() {
+    const grupoBot = new GrupoBot();
+});
