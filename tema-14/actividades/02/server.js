072e7415e9b0a998ae08cebc2039fb536de803c0
dgonrom2803 <dgonrom2803@g.educaand.es>
2024-02-22 10:34:14 +0100

    Tarea1 T15

--- tema-14/actividades/02/server.js
+++ tema-14/actividades/02/server.js
@@ -1,43 +1,58 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
 import express from "express";
 import path from "path";
+import { MongoClient } from "mongodb";
 
 const uri =
   "mongodb+srv://dgonrom2803:dgonrom2803@gesdwec.mka3zc2.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri);
 
 const app = express();
-const router = express.Router();
-var __dirname = path.resolve(); //Resuelve y adapta para módulos ES6
+var __dirname = path.resolve();
 
-async function run() {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Esta es la ruta para agregar registros
+app.post("/agregar-registro", async (req, res) => {
   try {
-    await client.connect();
+    const registro = req.body;
+    const queryAll = await run(registro);
+    res.json(queryAll);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+});
 
-    const database = client.db("Alumnos");
-    const collection = database.collection("2DAW");
 
-    // Realiza una operación de lectura (por ejemplo, encuentra todos los documentos en la colección)
-    const documentos = await collection.find({}).toArray();
-    console.log("Documentos en la colección '2DAW':");
-    console.log(documentos);
-  } finally {
-    await client.close();
+async function run(registro) {
+  try {
+    const database = client.db('Alumnos');
+    const datos = database.collection('2DAW');
+    
+    // Inserta registros en la base de datos si el nombre no está vacío
+    if (registro.nombre !== "") {
+      await datos.insertOne(registro);
+    }
+
+    // Consulta todos los documentos y espera a que se resuelva la promesa
+    const queryAll = await datos.find().toArray();
+    
+    // Devuelve los documentos consultados
+    return queryAll;
+  } catch (error) {
+    console.error(error);
+    
+    throw error;
   }
 }
 
-run().catch(console.dir);
-router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.listen(3000, () => {
+  console.log("Escuchando en puerto 3000");
 });
-app.use("/", router);
-app.use(express.static(__dirname)); //IMPORTANTE carga archivos js,css, etc.., cargados en los html desde directorio
-app.listen(3000);
-console.log("Escuchando en puerto 3000");
