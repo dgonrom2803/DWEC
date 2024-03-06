2dce0d83554a01ed8fb1f92cbb3e1788c7f65c8b
dgonrom2803 <dgonrom2803@g.educaand.es>
2024-03-06 13:03:17 +0100

    Ejemplo 5. NG-Bootstrap

--- tema-15/actividades/04/ajax/src/app/botonajax/botonajax.component.ts
+++ tema-15/actividades/04/ajax/src/app/botonajax/botonajax.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServiajaxService } from '../serviajax.service';
+import {OnInit ,Renderer2 ,ViewChild, ElementRef,} from '@angular/core';
 
 @Component({
   selector: 'app-botonajax',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './botonajax.component.css'
 })
 export class BotonajaxComponent {
+  @ViewChild("datos") idDiv: ElementRef|any;
+  constructor(private serviAjax: ServiajaxService, private rd: Renderer2 ) { }
+  llamaAjaxC():void{
+    this.serviAjax.llamaAjax('http://localhost/dwec/tema-15/actividades/04/ajax/src/app/jsonGET.php?nombre=Juan&ciudad=Ubrique')
+      .subscribe(data=>{
+        this.rd.setProperty(this.idDiv.nativeElement, 'innerHTML',`Desde servidor ${data['nombre']} de ${data['ciudad']}`)
+         
+        }) ;      
+ }
+
 
 }
